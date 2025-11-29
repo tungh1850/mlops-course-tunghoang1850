91d5fa90323b7e82b6feb5ed390ef0b60933c56a
Tung Hoang <tungh1850@gmail.com>
2025-11-29 13:55:40 +0000

    Revert "feat: auto deploy GCP"
    This reverts commit 0497428da5f5cad5ccae0c8ff07ff24df32ced48.

--- src/schemas/response.py
+++ src/schemas/response.py
@@ -3,7 +3,6 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-# Response schema for prediction results
 class PredictionResponse(BaseModel):
     FPD10_plus_probability: float
     model_name: str
